529e8b1b52a2f47bc5c697bccb27f5c9678d7352
Kacper Krok <kacperodespero.01@wp.pl>
2023-07-01 06:47:55 +0200

    Changed positioning of offer containers

--- src/components/offer/SingleOffer.js
+++ src/components/offer/SingleOffer.js
@@ -1,10 +1,13 @@
 import styles from "../../styles/SingleOffer.module.css";
 
-const SingleOffer = ({img,alt,title}) => {
+const SingleOffer = ({ img, alt, title }) => {
   return (
     <div className={styles["single-offer-container"]}>
+      <div className={styles["image-container"]}>
+        <img className={styles["image"]} src={img} alt={alt} />
+      </div>
+
       <p>{title}</p>
-      <img className={styles["single-offer"]} src={img} alt={alt} />
     </div>
   );
 };
