3fc32562e215f38349f1635b8c9d9596084c2468
Kacper Krok <kacperodespero.01@wp.pl>
2023-07-04 23:37:15 +0200

    Lazy loading

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Link } from "react-router-dom";
 import styles from "../../styles/Gallery.module.css";
+import LazyLoad from "react-lazyload";
 
 const SingleImage = React.lazy(() => import("./SingleImage"));
 
@@ -14,12 +15,14 @@ const Gallery = (props) => {
           <div key={index} className={styles["gallery-pic"]}>
             <Link to={`/project/${item.title}`}>
               <Suspense fallback={<div></div>}>
-                <SingleImage
-                  img={item.images}
-                  firstImg={item.images[0]}
-                  alt="item"
-                  title={item.title}
-                />
+                <LazyLoad once>
+                  <SingleImage
+                    img={item.images}
+                    firstImg={item.images[0]}
+                    alt="item"
+                    title={item.title}
+                  />
+                </LazyLoad>
               </Suspense>
             </Link>
           </div>
