68feca8e433dc557e5f2a313214da65c46f2b82d
Kacper Krok <kacperodespero.01@wp.pl>
2023-07-05 12:07:44 +0200

    Changed logo size in contact and navbar

--- src/components/contact/ContactContainer.js
+++ src/components/contact/ContactContainer.js
@@ -8,33 +8,25 @@ const ContactContainer = () => {
       </div>
 
       <div className={styles.logo}>
-        <img src="./logo.jpg" alt="logo" className={styles.logo}></img>
+        <img
+          src="./logo.jpg"
+          alt="logo"
+          className={styles["logo-content"]}
+        ></img>
       </div>
 
-      <div className={styles.owner}>
-        <p className={styles.para}>BIURO PROJEKTOWE</p>
-        <br></br>
-        <p className={styles.para}>ARCHITEKT MARIUSZ KROK</p>
-      </div>
-
-      <div className={styles.phone}>
-        <p className={styles.para}>TELEFON: 733996580 </p>
-      </div>
-
-      <div className={styles.email}>
-        <p className={styles.para}>E-MAIL: biuro@krokprojekt.pl</p>
-      </div>
-
-      <div className={styles.linkedin}>
-        <p className={styles.para}>
-          <a
-            href="https://www.linkedin.com/in/mariuszkrok/"
-            className={styles.link}
-          >
-            LINKEDIN : https://www.linkedin.com/in/mariuszkrok/
-          </a>
-        </p>
-      </div>
+      <p className={styles.para}>BIURO PROJEKTOWE</p>
+      <p className={styles.para}>ARCHITEKT MARIUSZ KROK</p>
+      <p className={styles.para}>TELEFON: 733996580 </p>
+      <p className={styles.para}>E-MAIL: biuro@krokprojekt.pl</p>
+      <p className={styles.para}>
+        <a
+          href="https://www.linkedin.com/in/mariuszkrok/"
+          className={styles.link}
+        >
+          LINKEDIN : https://www.linkedin.com/in/mariuszkrok/
+        </a>
+      </p>
     </div>
   );
 };
